b989d3d482931f871379047f0849680155b9230b
EricY216 <148021841+EricY216@users.noreply.github.com>
2023-11-25 22:39:12 +0800

    Update merchantControl.php

--- merchantControl.php
+++ merchantControl.php
@@ -7,27 +7,26 @@ case "listJob":
   $jobs=getJobList();
   echo json_encode($jobs);
   return;  
+//增加商品
 case "addJob":
-	
 	$jsonStr = $_POST['dat'];
 	$job = json_decode($jsonStr);
 	//should verify first
 	addJob($job->name,$job->price,$job->content);
 	return;
+//更新商品狀態
 case "updateJob":
-
 	$id=(int)$_REQUEST['id'];
 	$jsonStr = $_POST['dat'];
 	$job = json_decode($jsonStr);
 	updateJob($id,$job->name,$job->price,$job->content);
 	return;
+//刪除商品
 case "delJob":
 	$id=(int)$_REQUEST['id']; //$_GET, $_REQUEST
 	//verify
 	delJob($id);
 	return;
 default:
-  
 }
-
 ?>
