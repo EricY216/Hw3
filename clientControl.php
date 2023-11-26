a4b0e42d084104146f378dba2794270ca135a65a
EricY216 <148021841+EricY216@users.noreply.github.com>
2023-11-26 23:15:29 +0800

    Update clientControl.php

--- clientControl.php
+++ clientControl.php
@@ -8,26 +8,27 @@ case "listJob":
   $jobs=getJobList();
   echo json_encode($jobs);
   return;
-
+//加入商品
 case "addJob":
 	$jsonStr = $_POST['dat'];
 	$job = json_decode($jsonStr);
 	//should verify first
 	addJob($job->name,$job->price,$job->content,$job->number);
 	return;
+//刪除商品
 case "delJob":
 	$id=(int)$_REQUEST['id']; //$_GET, $_REQUEST
 	//verify
 	delJob($id);
 	return;
-
+//購物車列表
 case "listshopping":
 	$jobs=getJobList1();
 	echo json_encode($jobs);
 	return;
 /*
 case "updateJob":
-
+        //更新狀態
 	$id=(int)$_REQUEST['id'];
 	$jsonStr = $_POST['dat'];
 	$job = json_decode($jsonStr);
