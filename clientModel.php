aeebc16d5f16a45df82f295f64f3e0b4d0acdce8
EricY216 <148021841+EricY216@users.noreply.github.com>
2023-11-27 01:24:17 +0800

    Update clientModel.php

--- clientModel.php
+++ clientModel.php
@@ -5,14 +5,18 @@ function getJobList()
 {
 	global $db;
 	$sql = "select * from shop;";
-	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
-	mysqli_stmt_execute($stmt); //執行SQL
-	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
-
-	$rows = array(); //要回傳的陣列
+	//precompile sql指令，建立statement 物件，以便執行SQL
+	$stmt = mysqli_prepare($db, $sql ); 
+	//執行SQL
+	mysqli_stmt_execute($stmt); 
+	//取得查詢結果
+	$result = mysqli_stmt_get_result($stmt);
+        //要回傳的陣列
+	$rows = array(); 
 	while($r = mysqli_fetch_assoc($result))
 	{
-		$rows[] = $r; //將此筆資料新增到陣列中
+		//將此筆資料新增到陣列中
+		$rows[] = $r; 
 	}
 	return $rows;
 }
@@ -21,14 +25,18 @@ function getJobList1()
 {
 	global $db;
 	$sql = "select * from shopping;";
-	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
-	mysqli_stmt_execute($stmt); //執行SQL
-	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
-
-	$rows = array(); //要回傳的陣列
+	//precompile sql指令，建立statement 物件，以便執行SQL
+	$stmt = mysqli_prepare($db, $sql ); 
+	//執行SQL
+	mysqli_stmt_execute($stmt); 
+	//取得查詢結果
+	$result = mysqli_stmt_get_result($stmt); 
+        //要回傳的陣列
+	$rows = array(); 
 	while($r = mysqli_fetch_assoc($result))
 	{
-		$rows[] = $r; //將此筆資料新增到陣列中
+		//將此筆資料新增到陣列中
+		$rows[] = $r; 
 	}
 	return $rows;
 }
@@ -36,21 +44,28 @@ function getJobList1()
 function addJob($name,$price,$content,$number)
 {
 	global $db;
-    $total = $number*$price;
-	$sql = "insert into shopping (name, price, content, number, total) values (?, ?, ?, ?, ?)"; //SQL中的 ? 代表未來要用變數綁定進去的地方
-	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
-	mysqli_stmt_bind_param($stmt, "sisii", $name, $price,$content,$number,$total); //bind parameters with variables, with types "sss":string, string ,string
-	mysqli_stmt_execute($stmt);  //執行SQL
+        $total = $number*$price;
+	//SQL中的 ? 代表未來要用變數綁定進去的地方
+	$sql = "insert into shopping (name, price, content, number, total) values (?, ?, ?, ?, ?)"; 
+	//prepare sql statement
+	$stmt = mysqli_prepare($db, $sql); 
+	//bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_bind_param($stmt, "sisii", $name, $price,$content,$number,$total);
+	//執行SQL
+	mysqli_stmt_execute($stmt);  
 	return True;
 }
 function delJob($id)
 {
 	global $db;
-
-	$sql = "delete from shopping where id=?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
-	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
-	mysqli_stmt_bind_param($stmt, "i", $id); //bind parameters with variables, with types "sss":string, string ,string
-	mysqli_stmt_execute($stmt);  //執行SQL
+        //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$sql = "delete from shopping where id=?;"; 
+	//prepare sql statement
+	$stmt = mysqli_prepare($db, $sql); 
+	//bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_bind_param($stmt, "i", $id); 
+	//執行SQL
+	mysqli_stmt_execute($stmt);  
 	return True;
 }
 
@@ -58,11 +73,14 @@ function delJob($id)
 function updateJob($id, $name,$price,$content)
 {
 	global $db;
-
-	$sql = "update list set name=?, price=?, content=? where id=?"; //SQL中的 ? 代表未來要用變數綁定進去的地方
-	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
-	mysqli_stmt_bind_param($stmt, "sisi", $name, $price,$content, $id); //bind parameters with variables, with types "sss":string, string ,string
-	mysqli_stmt_execute($stmt);  //執行SQL
+        //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$sql = "update list set name=?, price=?, content=? where id=?"; 
+        //prepare sql statement
+	$stmt = mysqli_prepare($db, $sql); 
+        //bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_bind_param($stmt, "sisi", $name, $price,$content, $id); 
+        //執行SQL
+	mysqli_stmt_execute($stmt);  
 	return True;
 }
 */
